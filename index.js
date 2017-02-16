7e9bce8f26e4dd0414048da94c19a57aa557002d
ray851107 <ray851107@gmail.com>
2017-02-16 09:42:10 +0800

    use async/await

--- index.js
+++ index.js
@@ -5,41 +5,47 @@ const config = require('./config.json')
 
 const bot = new TelegramBot(config.bot.token, { polling: true })
 
-const googleSearch = query =>
-    request('https://www.googleapis.com/customsearch/v1', {
-        qs: {
-            key: config.cse.key,
-            cx: config.cse.cx,
-            q: query,
-            fileType: query.endsWith('.gif') ? 'gif' : undefined,
-            searchType: 'image',
-            hl: 'zh-TW',
-            num: 1
-        },
-        json: true
-    })
-    .then(body => body.items[0].link)
+async function googleSearch (query) {
+    const qs = {
+        key: config.cse.key,
+        cx: config.cse.cx,
+        q: query,
+        fileType: query.endsWith('.gif') ? 'gif' : undefined,
+        searchType: 'image',
+        hl: 'zh-TW',
+        num: 1
+    }
+    
+    const body = await request('https://www.googleapis.com/customsearch/v1', { qs, json: true })
+    
+    return body.items[0].link
+}
 
-const search = (function () {
+function addCache (search) {
     const cache = new Map()
-    return query => cache.has(query)
-        ? Promise.resolve(cache.get(query))
-        : googleSearch(query).then(link => {
-            cache.set(query, link)
-            return link
-        })
-}())
 
-const parse = (text) => text.match(/\S+\.(jpg|png|bmp|gif)/gi) || []
+    return query => {
+        if (cache.has(query)) {
+            return cache.get(query)
+        }
+        const promise = search(query)
+        cache.add(query, promise)
+        return promise
+    }
+}
 
-bot.on('text', ({chat, text}) => {
-    parse(text).forEach(query => {
-        search(query).then(link => {
-            if (query.endsWith('.gif')) {
-                bot.sendDocument(chat.id, link)
-            } else {
-                bot.sendPhoto(chat.id, link)
-            }
-        })
-    })
+const search = addCache(googleSearch)
+
+const parse = text => text.match(/\S+\.(jpg|png|bmp|gif)/gi) || []
+
+bot.on('text', async ({chat, text}) => {
+    Promise.all(queries.map(async (query) => {
+        const link = await search(query)
+        if (query.endsWith('.gif')) {
+            bot.sendDocument(chat.id, link)
+        } else {
+            bot.sendPhoto(chat.id, link)
+        }
+    }))
+    .catch(console.error)
 })
