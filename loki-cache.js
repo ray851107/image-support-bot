fd5a7092d4b706a4e032ba667906389174010a13
ray851107 <ray851107@gmail.com>
2018-03-05 14:01:34 +0800

    fix

--- loki-cache.js
+++ loki-cache.js
@@ -2,18 +2,24 @@ const Loki = require('lokijs')
 
 class LokiCache {
     constructor(filename, ttl = 24 * 60 * 60 * 1000) {
-        this.db = new Loki(filename, { autosave: true })
-        this.entries = this.db.addCollection('entries', {
-            unique: ['query'],
-            ttl: ttl,
-            ttlInterval: 24 * 60 * 60 * 1000
-        })
+        this.db = new Loki(filename, { autosave: true, autosaveInterval: 5000 })
+        this.entries = null
     }
 
-    load() {
-        return new Promise((resolve, reject) => {
+    async load() {
+        await new Promise((resolve, reject) => {
             this.db.loadDatabase({}, err => (err ? reject(err) : resolve()))
         })
+
+        this.entries = this.db.getCollection('entries')
+
+        if (this.entries == null) {
+            this.entries = this.db.addCollection('entries', {
+                unique: ['query'],
+                ttl: ttl,
+                ttlInterval: 24 * 60 * 60 * 1000
+            })
+        }
     }
 
     get(query) {
