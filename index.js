398767f8087589a7a8c0afa2781aee776622c0f9
ray851107 <16625236+ray851107@users.noreply.github.com>
2019-09-03 21:34:30 +0800

    Prettier

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const { customSearch, imageSearch } = require('./google')
 const config = require('./config.json')
 
 async function search(query) {
-    try {
-        return await customSearch(query)
-    } catch(e) {
-        return await imageSearch(query)
-    }
+  try {
+    return await customSearch(query)
+  } catch (e) {
+    return await imageSearch(query)
+  }
 }
 
 const parse = text => text.match(/\S+\.(jpg|png|bmp|gif)/gi) || []
@@ -18,31 +18,31 @@ const parse = text => text.match(/\S+\.(jpg|png|bmp|gif)/gi) || []
 const bot = new Telegraf(config.bot.token)
 
 bot.on('text', ({ message, telegram }) => {
-    const { text, chat } = message
-    const queries = parse(text).filter(match => !isUrl(match))
+  const { text, chat } = message
+  const queries = parse(text).filter(match => !isUrl(match))
 
-    queries.forEach(async query => {
-        try {
-            const link = await search(query)
-            if (query.endsWith('.gif')) {
-                await telegram.sendDocument(chat.id, link)
-            } else {
-                await telegram.sendPhoto(chat.id, link)
-            }
-        } catch (err) {
-            console.error(err)
-        }
-    })
+  queries.forEach(async query => {
+    try {
+      const link = await search(query)
+      if (query.endsWith('.gif')) {
+        await telegram.sendDocument(chat.id, link)
+      } else {
+        await telegram.sendPhoto(chat.id, link)
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  })
 })
 
 bot.catch(console.error)
 
 async function main() {
-    try {
-        bot.startPolling()
-    } catch (err) {
-        console.error(err)
-    }
+  try {
+    bot.startPolling()
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 main()
