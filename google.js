e153b7bcda16fd828b3910700dc961cfc1ff53fe
ray851107 <16625236+ray851107@users.noreply.github.com>
2020-02-05 14:14:02 +0800

    Fix image search

--- google.js
+++ google.js
@@ -1,5 +1,7 @@
 const fetch = require('node-fetch')
 const htmlparser2 = require('htmlparser2')
+const domhandler = require('domhandler')
+const domutils = require('domutils')
 const qs = require('querystring')
 
 const config = require('./config.json')
@@ -51,53 +53,70 @@ async function imageSearch(query) {
     { headers }
   )
 
-  const data = await parseImageSearch(res.body)
+  const dom = await parseHtml(res.body)
 
-  return data.ou
+  const url = getUrlFromScript(dom) || getUrlFromRgMeta(dom)
+
+  if (url == null) {
+    throw new Error('image url not found')
+  }
+
+  return url
 }
 
-function parseImageSearch(stream) {
+function parseHtml(stream) {
   return new Promise((resolve, reject) => {
-    let parsing = false
-    let found = false
-    let result = ''
-
-    const parser = new htmlparser2.WritableStream({
-      onopentag(name, attributes) {
-        if (found) return
-        const className = attributes.class
-        if (className && /(^|\s)rg_meta($|\s)/.test(className)) {
-          parsing = true
-        }
-      },
-      ontext(text) {
-        if (parsing) result += text
-      },
-      onclosetag() {
-        if (!parsing) return
-
-        found = true
-        parsing = false
-
-        stream.destroy()
-        parser.destroy()
-
-        try {
-          resolve(JSON.parse(result))
-        } catch (err) {
-          reject(err)
-        }
-      },
-      onend() {
-        if (!found) reject(new Error('link not found'))
-      },
-      onerror(err) {
-        if (!found) reject(err)
+    const handler = new domhandler.DomHandler((err, dom) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(dom)
       }
     })
-
+    const parser = new htmlparser2.WritableStream(handler)
     stream.pipe(parser)
   })
+}
+
+function getUrlFromScript(dom) {
+  const scripts = domutils
+    .findAll(e => e.name === 'script', dom)
+    .map(elem => domutils.getText(elem))
+  const prefix =
+    "AF_initDataCallback({key: 'ds:2', isError:  false , hash: '3', data:function(){return "
+  const suffix = '}});'
+  for (const script of scripts) {
+    if (script.startsWith(prefix) && script.endsWith(suffix)) {
+      const json = script.substring(
+        prefix.length,
+        script.length - suffix.length
+      )
+      const data = JSON.parse(json)
+      const url = get(data, [31, 0, 12, 2, 0, 1, 3, 0])
+      return typeof url === 'string' ? url : null
+    }
+  }
+}
+
+function getUrlFromRgMeta(dom) {
+  const elem = domutils.findOne(e => {
+    const className = domutils.getAttributeValue(e, 'class')
+    return /(^|\s)rg_meta($|\s)/.test(className)
+  }, dom)
+  if (elem != null) {
+    const json = domutils.getText(elem)
+    return JSON.parse(json).ou
+  } else {
+    return null
+  }
+}
+
+function get(data, path) {
+  for (let i = 0, len = path.length; i < len; ++i) {
+    if (data == null) break
+    data = data[path[i]]
+  }
+  return data
 }
 
 module.exports = { customSearch, imageSearch }
